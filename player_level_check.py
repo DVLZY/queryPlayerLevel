be4afc1e21242cb21c5b62f719dd464ffe66f66d
linzeyu <linzeyu@live.com>
2024-12-05 22:26:28 +0800

    修订字符串格式化用法

--- player_level_check.py
+++ player_level_check.py
@@ -7,7 +7,7 @@ for name in l_name.split(' '):
     # 获取运动员证书编号
     headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
     response = requests.request("GET"
-                                , "https://zwfw.sport.gov.cn/level.do?m=getLevelList&item=36.1&name={}".format(name)
+                                , f"https://zwfw.sport.gov.cn/level.do?m=getLevelList&item=36.1&name={name}"
                                 , headers=headers)
 
     if response.json()['data']['data']['list_data'] is None:
@@ -19,13 +19,13 @@ for name in l_name.split(' '):
             'content-type': 'application/json;charset=UTF-8',
             'requestid': f"{random.randint(1_000_000_000, 9_999_999_999):010d}"  # 逆向生成 requestid 算法
         }
-        payload = "{\"certificateNo\":\"{}\",\"athleteRealName\":\"{}\"}".format(list_data['certificate_num'], name)
+        payload = f'{{"certificateNo":"{list_data['certificate_num']}","athleteRealName":"{name}"}}'
         response = requests.request("POST"
                                     , url="https://ydydj.univsport.com/api/system/athlete/front-end-list"
                                     , headers=headers
                                     , data=payload)
         # 获取运动员信息详情
-        payload = "{\"athleteInfoId\":\"{}\"}".format(response.json()['data']['list'][0]['athleteInfoId'])
+        payload = f'{{"athleteInfoId":"{response.json()['data']['list'][0]['athleteInfoId']}"}}'
         response = requests.request("POST"
                                     , url="https://ydydj.univsport.com/api/system/athlete/front-end-detail"
                                     , headers=headers
@@ -33,7 +33,7 @@ for name in l_name.split(' '):
 
         # 组装信息
         rank_tilte = response.json()['data']['rankTitle']  # 运动等级
-        athlete_near_pic = "https://ydydj.univsport.com/{}".format(response.json()['data']['oneInchPhoto'])  # 照片
+        athlete_near_pic = f"https://ydydj.univsport.com/{response.json()['data']['oneInchPhoto']}"  # 照片
         event_time = response.json()['data']['eventTime']  # 时间
         event_name = response.json()['data']['eventName']  # 比赛
         small_item_name = response.json()['data']['smallItemName']  # 项目
