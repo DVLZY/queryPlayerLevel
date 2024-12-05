8c995c7608666ea225db890abee821d32673a44e
linzeyu <linzeyu@live.com>
2024-12-05 22:06:51 +0800

    更新接口

--- player_level_check.py
+++ player_level_check.py
@@ -1,28 +1,42 @@
+import random
 import requests
-import json
-from bs4 import BeautifulSoup
 
-headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 l_name = "林丹 谌龙 辣子鸡 石宇奇"
 for name in l_name.split(' '):
+
+    # 获取运动员证书编号
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
     response = requests.request("GET"
-                                , "https://ydydj.univsport.com/index.php?a=seach_look&item=36.1&user_name=%s" % name
+                                , "https://zwfw.sport.gov.cn/level.do?m=getLevelList&item=36.1&name={}".format(name)
                                 , headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.select_one('div.jieguo_main i').get_text()
-    if content != "0":
-        crt_id = soup.select_one('div.main_lista p').get('id')
+
+    if response.json()['data']['data']['list_data'] is None:
+        print(f"[{name}]\t[未找到]")
+        continue
+    for list_data in response.json()['data']['data']['list_data']:
+        # 获取运动员信息编号
+        headers = {
+            'content-type': 'application/json;charset=UTF-8',
+            'requestid': f"{random.randint(1_000_000_000, 9_999_999_999):010d}"  # 逆向生成 requestid 算法
+        }
+        payload = "{\"certificateNo\":\"{}\",\"athleteRealName\":\"{}\"}".format(list_data['certificate_num'], name)
         response = requests.request("POST"
-                                    , "https://ydydj.univsport.com/index.php?a=get_detail"
+                                    , url="https://ydydj.univsport.com/api/system/athlete/front-end-list"
                                     , headers=headers
-                                    , data="id=" + crt_id)
-        rank_tilte = json.dumps(response.json()['data']['rank_tilte'], indent=4, ensure_ascii=False)  # 运动等级
-        athlete_near_pic = json.dumps(response.json()['data']['athlete_near_pic'], indent=4, ensure_ascii=False)  # 照片
-        event_time = json.dumps(response.json()['data']['event_time'], indent=4, ensure_ascii=False)  # 时间
-        event_name = json.dumps(response.json()['data']['event_name'], indent=4, ensure_ascii=False)  # 比赛
-        small_item_name = json.dumps(response.json()['data']['small_item_name'], indent=4, ensure_ascii=False)  # 项目
-        event_grade = json.dumps(response.json()['data']['event_grade'], indent=4, ensure_ascii=False)  # 名次
+                                    , data=payload)
+        # 获取运动员信息详情
+        payload = "{\"athleteInfoId\":\"{}\"}".format(response.json()['data']['list'][0]['athleteInfoId'])
+        response = requests.request("POST"
+                                    , url="https://ydydj.univsport.com/api/system/athlete/front-end-detail"
+                                    , headers=headers
+                                    , data=payload)
+
+        # 组装信息
+        rank_tilte = response.json()['data']['rankTitle']  # 运动等级
+        athlete_near_pic = "https://ydydj.univsport.com/{}".format(response.json()['data']['oneInchPhoto'])  # 照片
+        event_time = response.json()['data']['eventTime']  # 时间
+        event_name = response.json()['data']['eventName']  # 比赛
+        small_item_name = response.json()['data']['smallItemName']  # 项目
+        event_grade = response.json()['data']['eventGrade']  # 名次
         print(
             f"[{name}]\t[{rank_tilte}]\t[{event_time}{event_name}{small_item_name}{event_grade}]\t[{athlete_near_pic}]")
-    else:
-        print(f"[{name}]\t[未找到]")
